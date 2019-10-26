6b93aa30a5ee9a386d684575a555992c7b32de14
Kartika <kartika02nair@gmail.com>
2019-10-26 13:51:23 +0530

    qr update maps plugin

--- www/js/index.js
+++ www/js/index.js
@@ -3,3 +3,32 @@ $(document).ready(function(){
     $('.collapsible').collapsible();
     $('.parallax').parallax();
 });
+
+cordova.plugins.barcodeScanner.scan(
+    function (result) {
+        alert("A barcode has been scanned \n" +
+              "Result: " + result.text + "\n" +
+              "Format: " + result.format + "\n" +
+              "Cancelled: " + result.cancelled);
+    }, 
+    function (error) {
+        alert("Scanning failed: " + error);
+    }
+);
+cordova.plugins.barcodeScanner.scan(
+    function (result) {
+         if(!result.cancelled){
+                if(result.format == "QR_CODE"){
+                     var value = result.text;
+                     console.log(value);
+                }else{
+                   alert("Sorry, only qr codes this time ;)");
+                }
+         }else{
+           alert("The user has dismissed the scan");
+         }
+      },
+      function (error) {
+           alert("An error ocurred: " + error);
+      }
+ );
